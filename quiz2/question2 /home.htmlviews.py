184bb93de7db2a418437da3940d5b1b2cc154c49
Suraiya Akter <suraiyasetu90@gmail.com>
2025-10-29 11:36:25 +0600

    Update home.htmlviews.py

--- quiz2/question2 /home.htmlviews.py
+++ quiz2/question2 /home.htmlviews.py
@@ -9,7 +9,4 @@
 </body>
 </html>from django.shortcuts import render
 
-def home(request):
-    name = "Abdur Rahim"
-    age = 22
-    return render(request, 'home.html', {'name': name, 'age': age})
+
